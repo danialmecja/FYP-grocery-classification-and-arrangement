78fc92f783c898cdfaace5dd64fbb2bd4ea5244a
madelineyee <madeline.yee2000@gmail.com>
2023-01-16 15:04:47 +0800

    updating image capture to size estimation

--- grocery-size-estimation/size-estimation.py
+++ grocery-size-estimation/size-estimation.py
@@ -7,116 +7,137 @@ import argparse
 import imutils
 import cv2
 
+
+def take_picture():
+    cam = cv2.VideoCapture(2) # camport = 2 for jupiter 
+    result, image = cam.read()
+    
+    # If image will detected without any error,
+    # show result
+    if result:
+        return image
+
+    # If captured image is corrupted, moving to else part
+    else:
+        print("No image detected. Please! try again")
+    
+    
+
+def image_preprocessing(image):
+    
+    # Convert image to grayscale and blur slighly
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (7, 7), 0)
+    
+    # Perform edge detection, dilation, erotion
+    edged = cv2.Canny(gray, 50, 100)
+    edged = cv2.dilate(edged, None, iterations=5) 
+    edged = cv2.erode(edged, None, iterations=1)
+
+    
+    return edged
+
 def midpoint(ptA, ptB):
 	return ((ptA[0] + ptB[0]) * 0.5, (ptA[1] + ptB[1]) * 0.5)
-
-# construct the argument parse and parse the arguments
-#ap = argparse.ArgumentParser()
-#ap.add_argument("-i", "--image", required=True,
-#	help="path to the input image")
-#ap.add_argument("-w", "--width", type=float, required=True,
-#	help="width of the left-most object in the image (in inches)")
-#args = vars(ap.parse_args())
-
-# load the image, convert it to grayscale, and blur it slightly
-#image = cv2.imread(args["image"]) #read reference image
-image = cv2.imread('FYP-grocery-classification-and-arrangement/grocery-size-estimation/imagefiles/sample6.jpg')
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-gray = cv2.GaussianBlur(gray, (7, 7), 0)
-cv2.imshow("Gray",gray)
-
-# perform edge detection, then perform a dilation + erosion to
-# close gaps in between object edges
-# tune iterations to match data images
-edged = cv2.Canny(gray, 50, 100)
-edged = cv2.dilate(edged, None, iterations=5) 
-edged = cv2.erode(edged, None, iterations=1)
-cv2.imshow("edge",edged)
-
-# find contours in the edge map
-cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL,
+    
+def get_size(ori_img, prep_img,width):
+    # Find contours in the edge map
+    cnts = cv2.findContours(prep_img.copy(), cv2.RETR_EXTERNAL,
 	cv2.CHAIN_APPROX_SIMPLE)
-cnts = imutils.grab_contours(cnts)
-#print(cnts)
+    cnts = imutils.grab_contours(cnts)
+    
+    (cnts, _) = contours.sort_contours(cnts) # sort the contours from left-to-right
 
-# sort the contours from left-to-right and initialize the
-# 'pixels per metric' calibration variable
-(cnts, _) = contours.sort_contours(cnts)
-pixelsPerMetric = None
+    pixelsPerMetric = None # 'pixels per metric' calibration variable
 
-# loop over the contours individually
-for c in cnts:
-	# if the contour is not sufficiently large, ignore it
-	if cv2.contourArea(c) < 1000:
-		continue
+    # loop over the contours individually
+    for c in cnts:
+        # if the contour is not sufficiently large, ignore it
+        if cv2.contourArea(c) < 1000: # edit this according to dataset
+            continue
 
-	# compute the rotated bounding box of the contour
-	orig = image.copy()
-	box = cv2.minAreaRect(c)
-	box = cv2.cv.BoxPoints(box) if imutils.is_cv2() else cv2.boxPoints(box)
-	box = np.array(box, dtype="int")
+        # compute the rotated bounding box of the contour
+        orig = ori_img.copy()
+        box = cv2.minAreaRect(c)
+        box = cv2.cv.BoxPoints(box) if imutils.is_cv2() else cv2.boxPoints(box)
+        box = np.array(box, dtype="int")
 
-	# order the points in the contour such that they appear
-	# in top-left, top-right, bottom-right, and bottom-left
-	# order, then draw the outline of the rotated bounding
-	# box
-	box = perspective.order_points(box)
-	cv2.drawContours(orig, [box.astype("int")], -1, (0, 255, 0), 2)
-	# loop over the original points and draw them
-	for (x, y) in box:
-		cv2.circle(orig, (int(x), int(y)), 5, (0, 0, 255), -1)
- 
- 
-  # unpack the ordered bounding box, then compute the midpoint
-	# between the top-left and top-right coordinates, followed by
-	# the midpoint between bottom-left and bottom-right coordinates
-	(tl, tr, br, bl) = box
-	(tltrX, tltrY) = midpoint(tl, tr)
-	(blbrX, blbrY) = midpoint(bl, br)
-  
-  # compute the midpoint between the top-left and bottom-left points,
-	# followed by the midpoint between the top-righ and bottom-right
-	(tlblX, tlblY) = midpoint(tl, bl)
-	(trbrX, trbrY) = midpoint(tr, br)
+        # order the points in the contour such that they appear
+        # in top-left, top-right, bottom-right, and bottom-left
+        # order, then draw the outline of the rotated bounding
+        # box
+        box = perspective.order_points(box)
+        cv2.drawContours(orig, [box.astype("int")], -1, (0, 255, 0), 2)
+        # loop over the original points and draw them
+        for (x, y) in box:
+            cv2.circle(orig, (int(x), int(y)), 5, (0, 0, 255), -1)
 
-  # draw the midpoints on the image
-	cv2.circle(orig, (int(tltrX), int(tltrY)), 5, (255, 0, 0), -1)
-	cv2.circle(orig, (int(blbrX), int(blbrY)), 5, (255, 0, 0), -1)
-	cv2.circle(orig, (int(tlblX), int(tlblY)), 5, (255, 0, 0), -1)
-	cv2.circle(orig, (int(trbrX), int(trbrY)), 5, (255, 0, 0), -1)
+        # unpack the ordered bounding box, then compute the midpoint
+            # between the top-left and top-right coordinates, followed by
+            # the midpoint between bottom-left and bottom-right coordinates
+            (tl, tr, br, bl) = box
+            (tltrX, tltrY) = midpoint(tl, tr)
+            (blbrX, blbrY) = midpoint(bl, br)
+        
+        # compute the midpoint between the top-left and bottom-left points,
+            # followed by the midpoint between the top-righ and bottom-right
+            (tlblX, tlblY) = midpoint(tl, bl)
+            (trbrX, trbrY) = midpoint(tr, br)
 
-  # draw lines between the midpoints
-	cv2.line(orig, (int(tltrX), int(tltrY)), (int(blbrX), int(blbrY)),
-		(255, 0, 255), 2)
-	cv2.line(orig, (int(tlblX), int(tlblY)), (int(trbrX), int(trbrY)),
-		(255, 0, 255), 2)
+        # draw the midpoints on the image
+            cv2.circle(orig, (int(tltrX), int(tltrY)), 5, (255, 0, 0), -1)
+            cv2.circle(orig, (int(blbrX), int(blbrY)), 5, (255, 0, 0), -1)
+            cv2.circle(orig, (int(tlblX), int(tlblY)), 5, (255, 0, 0), -1)
+            cv2.circle(orig, (int(trbrX), int(trbrY)), 5, (255, 0, 0), -1)
+        
+        # draw lines between the midpoints
+            cv2.line(orig, (int(tltrX), int(tltrY)), (int(blbrX), int(blbrY)),
+                (255, 0, 255), 2)
+            cv2.line(orig, (int(tlblX), int(tlblY)), (int(trbrX), int(trbrY)),
+                (255, 0, 255), 2)
 
-  # compute the Euclidean distance between the midpoints
-	dA = dist.euclidean((tltrX, tltrY), (blbrX, blbrY))
-	dB = dist.euclidean((tlblX, tlblY), (trbrX, trbrY))
- 
-  # if the pixels per metric has not been initialized, then
-	# compute it as the ratio of pixels to supplied metric
-	# (in this case, inches)
-	if pixelsPerMetric is None:
-		pixelsPerMetric = dB / 8.5	
-  #  pixelsPerMetric = dB / args["width"]
+        # compute the Euclidean distance between the midpoints
+            dA = dist.euclidean((tltrX, tltrY), (blbrX, blbrY))
+            dB = dist.euclidean((tlblX, tlblY), (trbrX, trbrY))
 
-	# compute the size of the object
-	dimA = dA / pixelsPerMetric
-	dimB = dB / pixelsPerMetric
-	print("Width:" , dimA)
-	print("Width:" , dimB)  
-	# draw the object sizes on the image
-	cv2.putText(orig, "{:.1f}cm".format(dimA),
-		(int(tltrX - 15), int(tltrY - 10)), cv2.FONT_HERSHEY_SIMPLEX,
-		0.65, (255, 255, 255), 2)
-	cv2.putText(orig, "{:.1f}cm".format(dimB),
-		(int(trbrX + 10), int(trbrY)), cv2.FONT_HERSHEY_SIMPLEX,
-		0.65, (255, 255, 255), 2)
-	
-  # show the output image
-	#cv2.imshow("Image", orig)
-  
-	cv2.imshow("Image",orig)
-	cv2.waitKey(0)
+        # Initialize if pPM is none
+            if pixelsPerMetric is None:
+                pixelsPerMetric = dB / width
+
+        # compute the size of the object
+            dimA = dA / pixelsPerMetric
+            dimB = dB / pixelsPerMetric
+            print("Width:" , dimA)
+            print("Width:" , dimB)  
+
+        # draw the object sizes on the image
+            cv2.putText(orig, "{:.1f}cm".format(dimA),
+                (int(tltrX - 15), int(tltrY - 10)), cv2.FONT_HERSHEY_SIMPLEX,
+                0.65, (255, 255, 255), 2)
+            cv2.putText(orig, "{:.1f}cm".format(dimB),
+                (int(trbrX + 10), int(trbrY)), cv2.FONT_HERSHEY_SIMPLEX,
+                0.65, (255, 255, 255), 2)
+
+        cv2.imshow("Image",orig)
+        cv2.waitKey(0)
+
+def main():
+
+    # Read file
+    #img_path = '/home/mustar/mad_fyp/FYP-grocery-classification-and-arrangement/grocery-size-estimation/imagefiles/sample1.jpg'
+    #ori_img = cv2.imread(img_path)
+
+    ori_img = take_picture()
+
+    width = 8.5
+    # Pre-process Image 
+    prep_img = image_preprocessing(ori_img)
+
+    # Perform size estimation
+    output = get_size(ori_img, prep_img, 5.0)
+
+    #cv2.imshow("output", output)
+    #cv2.waitKey(0)
+
+if __name__=="__main__":
+    main()
